5d7b95698f7a4d6601dbbd37daa414a9473ebf1c
DanielaIH <4427@holbertonschool.com>
2022-02-15 19:31:07 -0800

    ADD 100-times_table.c file that prints the n times table, starting with 0.

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int product;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				product = (i * j);
+				if (j == 0)
+					_putchar('0' + product);
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (product <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + product);
+					}
+					else if (product > 9 && product < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}
+					else if (product >= 100)
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
