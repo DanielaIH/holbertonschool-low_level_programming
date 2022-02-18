a1fe7247063828fee856a6046a81689226d5df16
DanielaIH <4427@holbertonschool.com>
2022-02-17 18:49:07 -0800

    ADD 5-more_numbers.c function that prints 10 times the numbers, from 0 to 14, followed by a new line

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *more_numbers - prints the numbers from 0
+ *to 14, 10 times
+ */
+void more_numbers(void)
+{
+	int times, num;
+
+	for (times = 0; times < 10; times++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar(num % 10 + '0');
+		}
+		_putchar(10);
+	}
+}
