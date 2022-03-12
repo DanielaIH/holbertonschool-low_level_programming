b921db64ffe8210288b34790f5f6ea866f4dacac
DanielaIH <4427@holbertonschool.com>
2022-03-11 20:44:55 -0800

    ADD 101-mul.c program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
-* main - multiply 2 numbers as arguments
+* main - multiplies tow positive numbers
 * @argc: number of arguments
 * @argv: arguments
 * Return: 0 success
@@ -12,17 +12,22 @@
 int main(int argc, char *argv[])
 {
 unsigned long mul;
-int i, j;
+int i = 0, j = 0;
+
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
 
 	}
