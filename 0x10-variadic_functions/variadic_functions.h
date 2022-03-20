8adfbed3cd7fa23462a0b32b6e10a4ce925b4128
DanielaIH <4427@holbertonschool.com>
2022-03-19 20:41:12 -0700

    ADD  3-print_all.c function that prints anything (char, integer, float, or string

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,18 @@
 #define _VARIADIC_H_
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+
+/**
+ * struct char_func - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+typedef struct char_func
+{
+	char letter;
+	void (*func)(va_list);
+} char_type;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
