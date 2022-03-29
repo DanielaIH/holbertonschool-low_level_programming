2e17b495ef56a69cf3690d1ba7cda02e155551dc
DanielaIH <4427@holbertonschool.com>
2022-03-29 16:08:55 -0500

    ADD 100-first.c function that is executed first than main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * before_main - execute before main function
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
