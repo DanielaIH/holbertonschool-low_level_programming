02802879d5f933777e7b27ad395d3b1af6eb9756
DanielaIH <4427@holbertonschool.com>
2022-04-07 18:08:30 -0500

    ADD 3-cp.c program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,40 +12,35 @@
 int main(int argc, char *argv[])
 {
 	int fd, fd_2, wr, rd, cl = 0;
-	char *buf;
+	char buf[1024];
 
 if (argc != 3)
-{
-	exit(97);
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-}
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
 fd = open(argv[1], O_RDONLY); /* open file from */
 if (fd == -1)
-{
-	close(fd);
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
-}
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+
 fd_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664); /*open file to*/
 if (fd_2 == -1)
-{
-	close(fd_2);
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	exit(99);
-}
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),	exit(99);
+
 if (cl == -1)
 {
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 	exit(100);
 }
-buf = malloc(sizeof(char) * strlen(argv[2]));
-if (buf == NULL)
-	return (0);
-rd = read(fd, buf, strlen(argv[2]));
-if (rd == -1)
-	return (0);
-wr = write(STDOUT_FILENO, buf, rd);
-if (wr == -1)
-	return (0);
+while (rd > 0)
+{
+	rd = read(fd, buf, 1024);
+	if (rd == -1)
+		return (0);
+
+	wr = write(fd_2, buf, rd);
+	if (wr == -1)
+		return (0);
+}
+close(fd);
+
 return (0);
 }
